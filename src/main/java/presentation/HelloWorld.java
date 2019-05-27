7d4e145ba692634ac6ae5b669f252c2e39cd1a01
Toshiya Kimura <tosikimu@gmail.com>
2019-05-28 01:11:09 +0900

    first commit

---
+++ src/main/java/presentation/HelloWorld.java
@@ -0,0 +1,11 @@
+package presentation;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		get("/hello", (req, res) -> "Hello World");
+	}
+
+}
